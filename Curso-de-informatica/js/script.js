2e90f873fe033e69499f00eaf96dba69e5519ea7
Kreivesler Riley da Silva Luciano <54329663+kreivesler@users.noreply.github.com>
2024-05-11 04:39:21 -0300

    Create script.js

---
+++ Curso-de-informatica/js/script.js
@@ -0,0 +1,26 @@
+const botao = document.getElementById("btn");
+
+botao.addEventListener("click", function() {
+  window.location.href = "https://www.asaas.com/c/31ee15ewkbb0vgnq";
+});
+
+document.querySelectorAll('a[href^="#sv"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+document.querySelectorAll('a[href^="#ct"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
