0b69468685a921783a281ac631e3afce7f08e1e5
Kreivesler Riley da Silva Luciano <54329663+kreivesler@users.noreply.github.com>
2024-06-19 16:47:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const botaoMenu = document.getElementById('menu');
+
+  if (botaoMenu) {
+      botaoMenu.addEventListener('click', function() {
+          const menuLateral = document.getElementById('sb-m');
+          if (menuLateral.style.display === 'none' || menuLateral.style.display === '') {
+              menuLateral.style.display = 'block';
+          } else {
+              menuLateral.style.display = 'none';
+          }
+      });
+  }
+});
+
 const botao = document.getElementById("btn");
 
 botao.addEventListener("click", function() {
